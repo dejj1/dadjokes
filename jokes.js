9f93decb99d3e50bf75b51f32cd5675e3452d2f4
Martin Podloucký <podlouckymartin@gmail.com>
2021-05-10 21:49:26 +0200

    URL to all avatars.

--- jokes.js
+++ jokes.js
@@ -1,6 +1,7 @@
 export const jokes = [
   {
     id: 'user01',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png',
     name: 'Neroxx',
     text:
       'The secret service isn\'t allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"',
@@ -9,6 +10,7 @@ export const jokes = [
   },
   {
     id: 'user02',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user02.png',
     name: 'wetcxjfsj',
     text:
       "Why did the invisible man turn down the job offer? He couldn't see himself doing it.",
@@ -17,6 +19,7 @@ export const jokes = [
   },
   {
     id: 'user03',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user03.png',
     name: 'Oracimaru',
     text:
       "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
@@ -25,6 +28,7 @@ export const jokes = [
   },
   {
     id: 'user04',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user04.png',
     name: 'radnaros',
     text:
       'Did you hear the news? FedEx and UPS are merging. They’re going to go by the name Fed-Up from now on.',
@@ -33,6 +37,7 @@ export const jokes = [
   },
   {
     id: 'user05',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user05.png',
     name: 'liness',
     text:
       'My daughter screeched, "Daaaaaad, you haven\'t listened to one word I\'ve said, have you!?" What a strange way to start a conversation with me...',
@@ -41,6 +46,7 @@ export const jokes = [
   },
   {
     id: 'user06',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user06.png',
     name: 'JIy4H',
     text:
       'My wife tried to unlatch our daughter\'s car seat with one hand and said, "How do one armed mothers do it?" Without missing a beat I replied, "Single handedly."',
@@ -49,6 +55,7 @@ export const jokes = [
   },
   {
     id: 'user07',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user07.png',
     name: 'Klina',
     text:
       "When a dad drives past a graveyard: Did you know that's a popular cemetery? Yep, people are just dying to get in there!",
@@ -57,6 +64,7 @@ export const jokes = [
   },
   {
     id: 'user08',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user08.png',
     name: 'eho22',
     text:
       'My friend keeps saying "cheer up man it could be worse, you could be stuck underground in a hole full of water." I know he means well.',
@@ -65,6 +73,7 @@ export const jokes = [
   },
   {
     id: 'user09',
+    avatar: 'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user09.png',
     name: 'GreatEvil',
     text:
       'The fattest knight at King Arthur’s round table was Sir Cumference. He acquired his size from too much pi.',
